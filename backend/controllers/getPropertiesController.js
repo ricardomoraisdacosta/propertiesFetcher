5e529ca04440a4a8cd0853085ae51ecfa3da8bc5
Ricardo da Costa <ricardoimdacosta@gmail.com>
2021-07-17 17:45:54 +0100

    BackEnd: Added address

--- backend/controllers/getPropertiesController.js
+++ backend/controllers/getPropertiesController.js
@@ -1,27 +1,25 @@
 const { response } = require("express");
 const cityProperties = require("../data/properties-2.json");
 
-exports.getProperties = (req,res) =>{
-        let parsedProperties  = cityProperties.properties.map((property)=>(
-            {
-                id: property.id,
-                name: property.name,
-                description: property.description,
-                lowestPricePerNight : (property.lowestPricePerNight.value/7.55).toFixed(2),
-                overview: property.overview,
-                overallRating: (property.overallRating ? property.overallRating.overall/10 : null ),
-                featured: property.isFeatured,
-            }
-        ));
-    
-       let finalProperties = {
-           location: cityProperties.location,
-           properties: parsedProperties,
-       }; 
+exports.getProperties = (req, res) => {
+  let parsedProperties = cityProperties.properties.map((property) => ({
+    id: property.id,
+    name: property.name,
+    description: property.description,
+    lowestPricePerNight: (property.lowestPricePerNight.value / 7.55).toFixed(2),
+    overview: property.overview,
+    overallRating: property.overallRating
+      ? property.overallRating.overall / 10
+      : null,
+    featured: property.isFeatured,
+    address1: property.address1,
+    address2: property.address2,
+  }));
 
-       res.json(finalProperties);
+  let finalProperties = {
+    location: cityProperties.location,
+    properties: parsedProperties,
+  };
+
+  res.json(finalProperties);
 };
-
-
-
-
