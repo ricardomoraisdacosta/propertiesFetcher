63a7d182420b776ae2704c548e77dd767d94c02c
Ricardo da Costa <ricardoimdacosta@gmail.com>
2021-07-17 12:25:07 +0100

    Backend: Filtered all unnecessary info Backend: Converted lowestPricePerNight to EUR Backend: converted rating to decimal

--- backend/controllers/getPropertiesController.js
+++ backend/controllers/getPropertiesController.js
@@ -1,8 +1,24 @@
 const { response } = require("express");
-const properties = require("../data/properties-2.json");
+const cityProperties = require("../data/properties-2.json");
 
 exports.getProperties = (req,res) =>{
-        res.json(properties);
+        let parsedProperties  = cityProperties.properties.map((property)=>(
+            {
+                name: property.name,
+                description: property.description,
+                lowestPricePerNight : property.lowestPricePerNight.value/7.55,
+                overview: property.overview,
+                overallRating: (property.overallRating ? property.overallRating.overall/10 : null ) ,
+                featured: property.isFeatured,
+            }
+        ));
+    
+       let finalProperties = {
+           location: cityProperties.location,
+           properties: parsedProperties,
+       }; 
+
+       res.json(finalProperties);
 };
 
 
