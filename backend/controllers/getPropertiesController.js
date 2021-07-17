53c826254079a16319ca816b8fecccc0f148d17f
Ricardo da Costa <ricardoimdacosta@gmail.com>
2021-07-17 14:11:32 +0100

    BackEnd: bugFix missing id

--- backend/controllers/getPropertiesController.js
+++ backend/controllers/getPropertiesController.js
@@ -4,11 +4,12 @@ const cityProperties = require("../data/properties-2.json");
 exports.getProperties = (req,res) =>{
         let parsedProperties  = cityProperties.properties.map((property)=>(
             {
+                id: property.id,
                 name: property.name,
                 description: property.description,
                 lowestPricePerNight : property.lowestPricePerNight.value/7.55,
                 overview: property.overview,
-                overallRating: (property.overallRating ? property.overallRating.overall/10 : null ) ,
+                overallRating: (property.overallRating ? property.overallRating.overall/10 : null ),
                 featured: property.isFeatured,
             }
         ));
