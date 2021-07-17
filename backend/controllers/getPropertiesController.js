33b8efc8ed4af43304cf7c2d962b241bad14ab04
Ricardo da Costa <ricardoimdacosta@gmail.com>
2021-07-17 14:30:01 +0100

    Backend: Two decimal cases in Price

--- backend/controllers/getPropertiesController.js
+++ backend/controllers/getPropertiesController.js
@@ -7,7 +7,7 @@ exports.getProperties = (req,res) =>{
                 id: property.id,
                 name: property.name,
                 description: property.description,
-                lowestPricePerNight : property.lowestPricePerNight.value/7.55,
+                lowestPricePerNight : (property.lowestPricePerNight.value/7.55).toFixed(2),
                 overview: property.overview,
                 overallRating: (property.overallRating ? property.overallRating.overall/10 : null ),
                 featured: property.isFeatured,
